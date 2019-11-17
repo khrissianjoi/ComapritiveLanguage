65a7c616d455328b6e650dd708a9c4313b1e3106
khrissianjoi <khrissianjoi@gmail.com>
2019-11-17 17:24:59 +0000

    queue

--- c_practice/first.c
+++ c_practice/first.c
@@ -3,16 +3,37 @@ using System;
 
 public class Queue
 {
-	public string[] dataSet = new string[1];  
+	public string[] items = new string[1];  
 	public Queue()
 	{
-		
+		this.items = items;
 	}
-	public void Main()
+	
+	public void enqueue(string item) {
+		Array.Resize(ref this.items, this.items.Length + 1);
+		this.items[this.items.GetUpperBound(0)] = item;
+	}
+	
+	public string dequeue() {
+		return this.items[-1];
+	}
+	public static void Main()
 	{
-		dataSet[0]= "hello";
-		Console.WriteLine(dataSet);
+		Queue q = new Queue();
+		string val = Console.ReadLine();
+		q.enqueue(val);
+		val = Console.ReadLine();
+		q.enqueue(val);
+		val = Console.ReadLine();
+		q.enqueue(val);
+		val = Console.ReadLine();
+		q.enqueue(val);
+		foreach(var elem in q.items)
+		{
+    		Console.WriteLine(elem);
+		}
 	}
 }
+
 
 // https://dotnetfiddle.net/
