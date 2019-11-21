45665f6bf01c3986f559cc5575d5d7fecdb6c9ac
khrissianjoi <khrissianjoi@gmail.com>
2019-11-21 16:03:50 +0000

    c# implementation

--- c_practice/first.c
+++ c_practice/first.c
@@ -1,39 +1,11 @@
-using System;
-					
+#include <stdio.h>
 
-public class Queue
+int main()
 {
-	public string[] items = new string[1];  
-	public Queue()
-	{
-		this.items = items;
-	}
-	
-	public void enqueue(string item) {
-		Array.Resize(ref this.items, this.items.Length + 1);
-		this.items[this.items.GetUpperBound(0)] = item;
-	}
-	
-	public string dequeue() {
-		return this.items[-1];
-	}
-	public static void Main()
-	{
-		Queue q = new Queue();
-		string val = Console.ReadLine();
-		q.enqueue(val);
-		val = Console.ReadLine();
-		q.enqueue(val);
-		val = Console.ReadLine();
-		q.enqueue(val);
-		val = Console.ReadLine();
-		q.enqueue(val);
-		foreach(var elem in q.items)
-		{
-    		Console.WriteLine(elem);
-		}
-	}
+    printf("Hello, world\n");
+    return 0;
 }
 
-
-// https://dotnetfiddle.net/
+// cc -c first.c "compile"
+// cc -o first first.c "create executable"
+// ./first "execute"	
