aa727c051c816ff470198f6ec483d3e4286f2bcd
khrissianjoi <khrissianjoi@gmail.com>
2019-11-23 13:01:16 +0000

    user inputs added

--- GoLang/todo.go
+++ GoLang/todo.go
@@ -6,11 +6,72 @@ import (
   "strings"
 )
 
-func event() {
+func getDate() []string {
 	fmt.Println("Please enter a date: dd/mm/yyyy")
-	var i int
-	m,_  := fmt.Scan(&i)
-	fmt.Println(m)
+	reader := bufio.NewReader(os.Stdin)
+	var dateInput string
+	dateInput, _ = reader.ReadString('\n')
+	date := strings.Split(strings.TrimSpace(dateInput),"/")
+	return date
+}
+
+func getStartTime() []string {
+	fmt.Println("Please enter a start time: hh:mm")
+	reader := bufio.NewReader(os.Stdin)
+	var startTimeInput string
+	startTimeInput, _ = reader.ReadString('\n')
+	startTime := strings.Split(strings.TrimSpace(startTimeInput),":")
+	return startTime
+}
+
+func getLocation() string {
+	fmt.Println("Please enter a location")
+	reader := bufio.NewReader(os.Stdin)
+	var locationInput string
+	locationInput, _ = reader.ReadString('\n')
+	location := strings.TrimSpace(locationInput)
+	return location
+}
+
+func getDuration() float64 {
+	fmt.Println("Please enter a the duration: hh.mm")
+	// reader := bufio.NewReader(os.Stdin)
+	var durationInput float64
+	_, err := fmt.Scanf("%f", &durationInput)
+	fmt.Println(err)
+	return durationInput
+
+}
+func getAssigned() []string {
+	fmt.Println("Please Enter the people assigned to this task, seperated my a comma ','")
+	var assignedInput string
+	reader := bufio.NewReader(os.Stdin)
+	assignedInput, _ = reader.ReadString('\n')
+	assigned := strings.Split(strings.TrimSpace(assignedInput),",")
+	return assigned
+
+}
+func event() {
+	fmt.Print("You have chosen to add event\n")
+	date := getDate()
+	startTime := getStartTime()
+	location := getLocation()
+	fmt.Println("The date is", date)
+	fmt.Println("The start time is", startTime)
+	fmt.Println("The location is", location)
+}
+
+func task() {
+	fmt.Print("You have chosen to add a task\n")
+	date := getDate()
+	startTime := getStartTime()
+	duration := getDuration()
+	assigned := getAssigned()
+	fmt.Println("The date is", date)
+	fmt.Println("The start time is", startTime)
+	fmt.Println("The duration is", duration)
+	fmt.Println("The people assigned to the task are", assigned)
+
 }
 
 func main() {
@@ -22,10 +83,9 @@ func main() {
   userInput, _ = reader.ReadString('\n')
   toDo := strings.ToLower(strings.TrimSpace(userInput))
   if toDo == "event" {
-	fmt.Print("You have chosen to add event\n")
 	event()
   } else {
-    fmt.Print("You have chosen to add something\n")
+	task()
   }
   fmt.Print("DONE")
 }
