16b6d6b215a11a4233dfe1d7ee8649dcdac5427b
Khrissian Joi Neuda <khrissianjoi@gmail.com>
2019-12-13 02:29:03 +0000

    syntax fix

--- LogicVSFunctional/longestPrefix.hs
+++ LogicVSFunctional/longestPrefix.hs
@@ -1,11 +1,14 @@
 
 longestPrefix :: [String] -> String
+
 longestPrefix (currentPrefix:lastString:[]) = prefixMatcher currentPrefix lastString
-longestPrefix (currentPrefix:nextString:remainingStrings) = longestPrefix (newPrefix:remainingStrings)
-    where newPrefix = prefixMatcher currentPrefix nextString
+longestPrefix (currentPrefix:nextString:remainingStrings) = longestPrefix (updatedPrefix:remainingStrings)
+    where updatedPrefix = prefixMatcher currentPrefix nextString
 
 prefixMatcher :: String -> String -> String
+
 prefixMatcher [] _ = ""
 prefixMatcher (currentPrefix:remainingPrefix) (currentChar:remainingChars)
     | currentPrefix == currentChar = [currentPrefix] ++ prefixMatcher remainingPrefix remainingChars 
     | otherwise = ""
+
